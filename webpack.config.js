edcf96a940b4c4f6366fd5dc0f30596d7b1350b2
Andrew McRobb <andrewmcrobb@gmail.com>
2018-03-07 07:26:27 -0700

    Added missing babel deps + added base setup.

---
+++ webpack.config.js
@@ -0,0 +1,115 @@
+'use strict';
+
+const webpack = require('webpack'),
+  ExtractTextPlugin = require('extract-text-webpack-plugin'),
+  HtmlWebpackPlugin = require('html-webpack-plugin'),
+  autoprefixer = require('autoprefixer'),
+  OfflinePlugin = require('offline-plugin'),
+  path = require('path');
+
+const ENV = process.env.NODE_ENV || 'development';
+const SRC_PATH = path.join(__dirname, 'src/');
+const DIST_PATH = path.join(__dirname, 'docs/');
+
+const isDevelopment = (ENV ==='development') ? true : false;
+
+module.exports = {
+    context: SRC_PATH,
+    entry: [
+        'babel-polyfill',
+        './app.js'
+    ],
+    mode: ENV,
+    resolve: {
+        extensions: ['.jsx', '.js', '.json', '.scss'],
+        modules: [
+          path.resolve(__dirname, "src/lib"),
+          path.resolve(__dirname, "node_modules"),
+          'node_modules'
+        ],
+    },
+    output: {
+        path: DIST_PATH,
+        filename: '[name]-[hash].min.js',
+        publicPath: '/'
+    },
+    module : {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test:  /\.(scss|css)$/,
+                include: [path.join(SRC_PATH, 'components')],
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: { modules: true, sourceMap: isDevelopment, importLoaders: 1, minimize: true }
+                        },
+                        {
+                            loader: `postcss-loader`,
+                            options: {
+                                sourceMap: isDevelopment,
+                                plugins: () => {
+                                    autoprefixer({ browsers: [ 'last 4 versions' ] });
+                                }
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: { sourceMap: isDevelopment }
+                        }
+                    ]
+                })
+            },
+            {
+                test:  /\.(scss|css)$/,
+                exclude: [path.join(SRC_PATH, 'components')],
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: { modules: true, sourceMap: isDevelopment, importLoaders: 1, minimize: true }
+                        },
+                        {
+                            loader: `postcss-loader`,
+                            options: {
+                                sourceMap: isDevelopment,
+                                plugins: () => {
+                                    autoprefixer({ browsers: [ 'last 4 versions' ] });
+                                }
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: { sourceMap: isDevelopment }
+                        }
+                    ]
+                })
+            }
+        ]
+    },
+    stats: { colors: true },
+    node: {
+      global: true,
+      process: false,
+      Buffer: false,
+      __filename: false,
+      __dirname: false,
+      setImmediate: false
+    },
+    devServer: {
+        port: process.env.PORT || 8080,
+        host: 'localhost',
+        publicPath: '/',
+        contentBase: './src',
+        historyApiFallback: true,
+        open: true,
+        openPage: ''
+      }
+};
