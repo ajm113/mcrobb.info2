8cea9b603308c8cbb0854648312326b90ce00747
Andrew McRobb <andrewmcrobb@gmail.com>
2018-03-08 12:31:03 -0700

    Improved links + added bio.

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -3,8 +3,21 @@ import { h } from 'preact';
 export default () => {
   return (
     <div>
-      <h1>About</h1>
-      <p>This is the About component.</p>
+        <h1>All About McRobb</h1>
+        <p>
+            Andrew McRobb is a professional software/web developer, currently living in Arizona, U.S.
+            Inside the industry for 5 amazing years. Working from small start ups to large clients such as ASU as a Sr Developer. Currently works for Axion Communications
+            as a remote software developer. Earned a certificate in Object Oriented Programming from Arizona State University,
+            certificate in 3D Animation from East Valley Institute of Technology.
+        </p>
+        <p>
+            Andrew McRobb also dwelled in other industries such as photography, residential
+            computer repair for a few years. Ether as a primary or secondary job as self ran businesses, and does freelance on and off.
+        </p>
+        <p>
+            Andrew enjoys bike rides, food, hikes, learning cutting edge new technology and always looks to make someone's day better.
+            Ether being a project for work, personal friend, or random stranger. Andrew's favorite technologies are Linux (Debian), PHP, JavaScript, Erlang, and C/C++.
+        </p>
     </div>
   );
 };
