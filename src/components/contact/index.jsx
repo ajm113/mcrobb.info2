7a8db5df3fb53bb1f41b4d809edf365176361c25
Andrew McRobb <andrewmcrobb@gmail.com>
2018-03-08 12:30:38 -0700

    Added contact form.

--- src/components/contact/index.jsx
+++ src/components/contact/index.jsx
@@ -1,10 +1,18 @@
 import { h } from 'preact';
 
+import style from './style.scss';
+
 export default () => {
-  return (
-    <div>
-      <h1>Contact</h1>
-      <p>This is the Contact component.</p>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Contact McRobb</h1>
+            <p><i>Please use the contact form. -- I know it seems lazy, but I get enough spam.</i></p>
+            <form action="https://formspree.io/andrewmcrobb@gmail.com" method="POST" class={style.myform}>
+                <input type="text" name="name" placeholder="Your name" />
+                <textarea name="message" placeholder="Your message"/>
+                <input type="email" name="_replyto" placeholder="Your email address" />
+                <button type="submit">Send</button>
+            </form>
+        </div>
+    );
 };
