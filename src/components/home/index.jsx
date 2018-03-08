9b64a5dedb68447fc8d47657898aaa5a4c6bff29
Andrew McRobb <andrewmcrobb@gmail.com>
2018-03-08 04:30:13 -0700

    Added content improve UI

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,10 +1,21 @@
 import { h } from 'preact';
+import { Link } from 'preact-router/match';
 
 export default () => {
-  return (
-    <div>
-      <h1>Home</h1>
-      <p>This is the Home component.</p>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Everything Andrew McRobb</h1>
+            <p><i>
+                Everything software! Front-end, back-end, DevOps, System Admin, Photographer, Newbie Car Repair Guy!
+            </i></p>
+            <p>
+                Welcome, this is just a little portfolio about myself and my work! I'm a remote developer who works on a bit of everything you can think of!
+                PHP, JavaScript, GoLang, Erlang, Node.js, and many more!
+            </p>
+            <p>
+                If you are a employeer. How about you checkout <Link href="/work">my work</Link>!
+            </p>
+
+        </div>
+    );
 };
