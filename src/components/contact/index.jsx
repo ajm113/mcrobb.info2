53dfac64045260aa87bc58857bd87046735679cd
Andrew McRobb <andrewmcrobb@gmail.com>
2018-03-17 02:39:02 -0700

    Removed textbox field.

--- src/components/contact/index.jsx
+++ src/components/contact/index.jsx
@@ -9,9 +9,9 @@ export default () => {
             <hr />
             <p><i>Please use the contact form.</i></p>
             <form action="https://formspree.io/andrewmcrobb@gmail.com" method="POST" class={style.myform}>
-                <input type="text" name="name" placeholder="Your name" />
-                <textarea name="message" placeholder="Your message"/>
-                <input type="email" name="_replyto" placeholder="Your email address" />
+                <input type="text" name="name" placeholder="Your name" equired="required" />
+                <input name="message" placeholder="Your message" maxlength="300" required="required" />
+                <input type="email" name="_replyto" placeholder="Your email address" equired="required" />
                 <button type="submit">Send</button>
             </form>
         </div>
